223b0fb475d6783d514ad0bc2fda1c2f6936ba8a
Walle <valters.jargans@gmail.com>
2023-06-10 21:13:45 +0300

    Added Centroid DTO

---
+++ MVC-leaflet/DB/CentroidDataDTO.cs
@@ -0,0 +1,65 @@
+﻿namespace MVC_leaflet.DB
+{
+    /// <summary>
+    /// AW_VIETA_CENTROIDS
+    /// This table is referenced in section 2.2.17 of the documentation.
+    /// Documentation Link: https://www.vzd.gov.lv/lv/media/6149/download?attachment 
+    /// </summary>
+    public class CentroidDataDTO
+    {
+        /// <summary>
+        /// Centroid Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// KODS
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// TIPS_CD
+        /// </summary>
+        public int TypeCD { get; set; }
+
+        /// <summary>
+        /// NOSAUKUMS
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// VKUR_CD
+        /// </summary>
+        public int AddressObjectCode { get; set; }
+
+        /// <summary>
+        /// VKUR_TIPS
+        /// </summary>
+        public int AddressObjectTypeCode { get; set; }
+
+        /// <summary>
+        /// STD
+        /// </summary>
+        public string AddressObjectDescription { get; set; }
+
+        /// <summary>
+        /// KOORD_X
+        /// </summary>
+        public decimal CentroidXCoordinate { get; set; }
+
+        /// <summary>
+        /// KOORD_Y 
+        /// </summary>
+        public decimal CentroidYCoordinate { get; set; }
+
+        /// <summary>
+        /// DD_E
+        /// </summary>
+        public decimal CentroidDDECoordinate { get; set; }
+
+        /// <summary>
+        /// DD_N
+        /// </summary>
+        public decimal CentroidDDNCoordinate { get; set; }
+    }
+}
